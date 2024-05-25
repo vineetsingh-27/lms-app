fff3eaed2e09fae078df91521063982628c277f5
Vineet <vineet199927@gmail.com>
2024-05-26 01:05:35 +0530

    Course ui creation completed

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,9 +1,14 @@
-const CoursesPage = () =>{
-    return (
-        <div>
-            Courses page!
-        </div>
-    )
-}
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+const CoursesPage = () => {
+  return (
+    <div className="p-6">
+      <Link href="/teacher/create">
+        <Button>New Course</Button>
+      </Link>
+    </div>
+  );
+};
 
 export default CoursesPage;
